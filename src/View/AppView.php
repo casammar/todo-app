b9720faebd5910df3075fc67b16397fc0c0bf2cd
casammar <chris.sammarco@gmail.com>
2019-05-14 21:34:33 -0400

    paginator template for bootstrap

--- src/View/AppView.php
+++ src/View/AppView.php
@@ -37,5 +37,6 @@ class AppView extends View
     public function initialize()
     {
         $this->loadHelper('User');
+        $this->loadHelper('Paginator', ['templates' => 'paginator-templates']);
     }
 }
